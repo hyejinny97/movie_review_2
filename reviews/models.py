73d7fa8858f07ce4b363918c5a1e7e413348a6fb
Myeonghak Lee <mhmh779@naver.com>
2022-10-07 16:47:30 +0900

    Design

--- reviews/models.py
+++ reviews/models.py
@@ -6,7 +6,10 @@ class Review(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
     movie_name = models.CharField(max_length=30)
-    grade = models.IntegerField(default=1, help_text="0~5사이 값으로 입력하세요", validators=[MinValueValidator(0),
-                                       MaxValueValidator(5)])
+    grade = models.IntegerField(
+        default=1,
+        help_text="0~5사이 값으로 입력하세요",
+        validators=[MinValueValidator(0), MaxValueValidator(5)],
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
