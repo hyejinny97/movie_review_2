31dbf87ff39034f2b0149124f0c13f5a64916d47
hyejinny97 <yhj970102@gmail.com>
2022-10-07 11:32:54 +0900

    Add read, create

---
+++ reviews/forms.py
@@ -0,0 +1,21 @@
+from tkinter import Widget
+from django import forms
+from .models import Review
+from django.utils.translation import gettext_lazy as _
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = '__all__'
+        labels = {
+            'title': _('리뷰 제목'),
+            'content': _('리뷰 내용'),
+            'movie_name': _('영화 이름'),
+            'grade': _('영화 평점'),
+        }
+        # text = '제목을 입력해주세요.'
+        # widgets = {
+        #     'title': forms.CharField(attrs={'placeholder': text})
+        # }
+
+
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ReviewForm
+from .models import Review
 
 # Create your views here.
+# 목록 페이지
+def index(request):
+    reviews = Review.objects.all().order_by('-pk')
+
+    context = {
+        'reviews': reviews,
+    }
+
+    return render(request, 'reviews/index.html', context)
+
+# 글 작성 페이지 및 데이터 생성
+def create(request):
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save()
+            return redirect('reviews:detail', review.pk)
+    elif request.method == 'GET':
+        form = ReviewForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'reviews/create.html', context)
+
+# 글 상세 페이지
+def detail(request, pk):
+    return render(request, 'reviews/detail.html')
